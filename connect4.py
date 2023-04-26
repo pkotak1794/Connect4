2f643108102d8d697a0dd4bd0ce24a5a4df6bf40
Priyanka L <78058333+pkotak1794@users.noreply.github.com>
2023-04-26 11:36:03 -0700

    Update connect4.py

--- connect4.py
+++ connect4.py
@@ -1,9 +1,16 @@
 from games import *
 
+class ConnectFour(TicTacToe):
+  def __init__(self, h=7, v=6, k=4):
+     TicTacToe.__init__(self, h, v, k)
+
+  def actions(self, state):
+     return [(x, y) for (x, y) in state.moves
+             if x == self.h or (x + 1 , y ) in state.board]
 
 
 
-
+  def alpha_beta_search(state, game):
 
 
 
