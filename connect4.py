0141b15e5636828071ff82a0a735ef4162512e85
Priyanka L <78058333+pkotak1794@users.noreply.github.com>
2023-05-07 11:10:41 -0700

    Update connect4.py

--- connect4.py
+++ connect4.py
@@ -2,20 +2,19 @@
 # add valid moves function
 # alpha-beta pruning algorithm 
 
-
-from games import GameState
-from games import alpha_beta_player
-from games import ConnectFour
-from games import Game
+#from games import ConnectFour, alpha_beta_player, GameState, Game
 import copy
-
+from games import *
 
 class GameState:
-    def __init__(self, to_move, utility, board, moves=None):
-        self.to_move = to_move
+    def __init__(self, player_to_move, utility, board, moves=None):
+        self.player_to_move = player_to_move
         self.utility = utility
         self.board = board
         self.moves = moves or []
+
+    def to_move(self):
+        return self.player_to_move
 
 class ConnectFour(Game):
     def __init__(self):
@@ -23,7 +22,8 @@ class ConnectFour(Game):
         self.rows = 7
         self.win_length = 4
         self.board = {(row, col): "." for row in range(self.rows) for col in range(self.cols)}
-        self.initial = GameState(to_move='X', utility=0, board=self.board, moves=[])
+        self.initial = GameState(player_to_move='X', utility=0, board=self.board, moves=[])
+        self.ai_player = alpha_beta_player
 
     def to_game(self, state):
         game = ConnectFour()
@@ -34,24 +34,27 @@ class ConnectFour(Game):
         return game
     
     def actions(self, state):
+        if state is None:
+            return []
         return [(row, col) for row in range(self.rows) for col in range(self.cols)
             if state.board[(row, col)] == "." and (row == self.rows - 1 or state.board[(row+1, col)] != ".")]
 
     def result(self, state, row=None, col=None):
-        if col not in range(self.cols):
+        if state is None:
+            return None
+        if col is None or col not in range(self.cols):
             return state
-        # find the lowest unoccupied row in the given column
         for r in range(self.rows-1, -1, -1):
             if state.board[(r, col)] == ".":
                 break
         else: # column is full
             return state
-        player = state.to_move
+        player = state.player_to_move
         new_board = copy.deepcopy(state.board)
         new_board[(r, col)] = player
         new_moves = state.moves + [(r, col)]
         new_to_move = 'O' if player == 'X' else 'X'
-        new_state = GameState(to_move=new_to_move, utility=self.compute_utility(new_board, (r, col), player),
+        new_state = GameState(player_to_move=new_to_move, utility=self.compute_utility(new_board, (r, col), player),
                               board=new_board, moves=new_moves)
         return new_state
 
@@ -78,13 +81,13 @@ class ConnectFour(Game):
             if all(board[(r, c)] != '.' for r in range(self.rows) for c in range(self.cols)):
                 return 0
             else:
-                return None
+                return 0
 
     def terminal_test(self, state):
+        #print(type(state))
+        if state is None:
+            return False
         return state.utility is not None
-
-    def to_move(self, state):
-        return state.to_move
 
     def display(self, state):
         board = state.board
@@ -94,25 +97,24 @@ class ConnectFour(Game):
                 print(board[(row, col)], end=" | ")
             print("")
 
-
 if __name__ == "__main__":
-    c4 = ConnectFour()
-    print(c4)
-    board = {(row, col): "." for row in range(c4.rows) for col in range(c4.cols)}
-    c4.initial = GameState(to_move='X', utility=c4.compute_utility(board, None, 'X'), board=board, moves=[])
-    state = c4.initial
-    while not c4.terminal_test(state):
-        c4.display(state)
-        if state.to_move == 'X':
-            col = int(input("Enter column: "))
-            state = c4.result(state, row=None,col=col)
+    game = ConnectFour()
+    print(game)
+    board = {(row, col): "." for row in range(game.rows) for col in range(game.cols)}
+    game.initial = GameState(player_to_move='X', utility=game.compute_utility(board, None, 'X'), board=board, moves=[])
+    state = game.initial
+    while not game.terminal_test(state):
+        game.display(state)
+        if state.to_move() == 'X':
+            col = int(input("Enter a column number: "))
+            state = game.result(state, row=None,col=col)
         else:
-            state = alpha_beta_player(state, c4)
-    c4.display(state)
-    utility = c4.compute_utility(state.board, None, state.to_move)
+            state = alpha_beta_player(game, state)
+    game.display(state)
+    utility = game.compute_utility(state.board, None, state.to_move)
     if utility == 1:
-        print("You won!")
+        print("Congratulations! You won!")
     elif utility == -1:
-        print("You lost!")
+        print("Awww, you lost...")
     else:
-        print("Draw!")
+        print("It's a Draw.")
