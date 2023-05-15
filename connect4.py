184a3179d71a62ab148dac70659c5037b7c4b414
Priyanka L <78058333+pkotak1794@users.noreply.github.com>
2023-05-14 21:02:20 -0700

    Update connect4.py

--- connect4.py
+++ connect4.py
@@ -2,165 +2,137 @@
 # Xuan Bui, Priyanka Lee, & Tiffany Tran
 
 # Connect4 game against an AI or human player on a 9x7 grid size board
-# using alpha-beta pruning algorithm & show player valid moves
+# using alpha-beta pruning (cutoff) algorithm & show player valid moves
 
-#from games import ConnectFour, alpha_beta_player, GameState, Game
 import copy
+import time
 from games import *
 
-# define GameState class to initialize variables
-class GameState:
-    def __init__(self, player_to_move, utility, board, moves=None): # constructor that takes 4 arguments
-        self.player_to_move = player_to_move # tracks turn of player/opponent with a string
-        self.utility = utility # keeps score of game (1 = player win, -1 = player loss, 0 = draw)
-        self.board = board  # current board state
-        self.moves = moves or [] # empty list if no value for moves
-
-    # track whose turn it is    
-    def to_move(self):
-        return self.player_to_move
-
-# class definition for game play implementation 
-class ConnectFour(Game):
+# create new class that inherits from ConnectFour class 
+class NewConnectFour(ConnectFour):
     def __init__(self):
-        self.cols = 9   # number of rows
-        self.rows = 7   # number of columns
-        self.win_length = 4 # number of tokens needed in a row to win 
-        self.board = {(row, col): "." for row in range(self.rows) for col in range(self.cols)}  # game board initialization
-        self.initial = GameState(player_to_move='X', utility=0, board=self.board, moves=[]) # initial game state & player 'X' to move
-        self.ai_player = alpha_beta_player  # AI opponent set to alpha_beta_player
+        ConnectFour.__init__(self, h=9, v=7, k=4)   # set board size to 9x7 and 4 in a row to win
 
-    # create new instance with same dimensions + win condition as previous
-    def to_game(self, state):
-        game = ConnectFour()
-        game.cols = self.cols
-        game.rows = self.rows
-        game.win_length = self.win_length
-        game.initial = state
-        return game
-    
-    # keep track of what actions are available 
-    def actions(self, state):
-        if state is None:
-            return []
-        # get game board and generate list of possible actions (all empty spots) 
-        board = state.board
-        actions = [(row, col) for row in range(self.rows) for col in range(self.cols)
-            if board[(row, col)] == "." and (row == self.rows - 1 or board[(row+1, col)] != ".")]
-        print("Valid actions:", actions)    # prints valid list of actions available 
-        return actions
-
-    # analyze current game state and create new game state after token is played 
-    def result(self, state, row=None, col=None):
-        if state is None:
-            return None
-        if col is None or col not in range(self.cols):  # if selected column is out of range
-            return state
-        # traverse column backwards 
-        for r in range(self.rows-1, -1, -1):
-            if state.board[(r, col)] == ".":    # if column is empty, save row number 
-                break
-        else: # if no space in selected column 
-            return state
-        player = state.player_to_move   # check which player 'X' or 'O' has moved 
-        new_board = copy.deepcopy(state.board)
-        new_board[(r, col)] = player    # update board with token on newly occupied space
-        new_moves = state.moves + [(r, col)]    # save move in state 
-        new_to_move = 'O' if player == 'X' else 'X' # check which players turn it is now
-        new_state = GameState(player_to_move=new_to_move, utility=self.compute_utility(new_board, (r, col), player),
-                                  board=new_board, moves=new_moves) # calculate new utility based on recent move 
-        if not self.actions(new_state):
-            new_state.utility = 0
-        return new_state
-    
-    # computes game results and checks for a win/loss 
-    def compute_utility(self, board, move, player):
-        if move is None:
-            return None
-        row, col = move
-        directions = [(1, 0), (0, 1), (1, 1), (1, -1)]  # possible direction of win scenarios
-        # checks for wins in all directions (vertical/horizontal/diagonal)
-        for d in directions:
-            count = 1
-            # diagonally (bottom-left to top-right)
-            for i in range(1, self.win_length):
-                r, c = row + d[0]*i, col + d[1]*i
-                if r < 0 or r >= self.rows or c < 0 or c >= self.cols or board[(r, c)] != player:
-                    break
-                count += 1
-            # diagonally (top-left to bottom right)
-            for i in range(1, self.win_length):
-                r, c = row - d[0]*i, col - d[1]*i
-                if r < 0 or r >= self.rows or c < 0 or c >= self.cols or board[(r, c)] != player:
-                    break
-                count += 1
-            if count >= self.win_length:
-                return 1 if player == 'X' else -1
-        if all(board[(r, c)] != '.' for r in range(self.rows) for c in range(self.cols)):   # if the game results in a tie
-            return 0
-        return None    # if this line is changed to return None it causes infinite recursion but causes automatic Draw as is
-
-    # checks to see if the game is still in progress or completed 
-    def terminal_test(self, state):
-        #print(type(state))
-        if state is None:   # game isn't over yet 
-            return False
-        return state.utility is not None    # game is over 
-
+    # update display format for game board 
     def display(self, state):
         board = state.board
-        for row in range(self.rows):
-            print(" | ", end="")
-            for col in range(self.cols):
-                print(board[(row, col)], end=" | ")
+        for x in range(1, self.h + 1):
+            for y in range(1, self.v + 1):
+                print('| {} '.format(board.get((x, y), '.')), end='')
+            print('|')
+        print('-' * (self.h * 4 + 1))
+
+    # evaluation function that counts pieces in a row and returns difference
+    def evaluate(self, state):
+        score = 0   # initialize score to 0
+        # iterate over board height/width 
+        for x in range(1, self.h + 1):
+            for y in range(1, self.v + 1):
+                piece = state.board.get((x, y), None)   # check if there is a piece at current position
+                if piece is None:
+                    continue    # if no piece, pass over
+                # if piece belongs to current player, add to score 
+                if piece == state.to_move:
+                    score += self.row_count(state, (x, y))
+                # if score belongs to opponent, subtract from player score 
+                else:
+                    score -= self.row_count(state, (x, y))
+        return score
+
+    # count number of pieces players have in a row 
+    def row_count(self, state, pos):
+        directions = [(1, 0), (0, 1), (1, 1), (1, -1)] # list of possible directions for counting pieces
+        counts = [self.dir_count(state, pos, d) for d in directions] # save result in list 
+        return max(counts)  # return max form the counts list 
+
+    # count number of pieces players have in specific position from given position
+    def dir_count(self, state, pos, direction):
+        player = state.board[pos]   # stores piece at given position
+        count = 0   # initialize count to 0
+        x, y = pos  # tuple split in x and y variables
+        dx, dy = direction  # direction split into dx, dy variables
+        # while dx+x and dy+y = piece stored in player variable
+        while state.board.get((x+dx, y+dy)) == player:
+            count += 1  # increment count
+            x, y = x+dx, y+dy   # update x & y 
+        return count
+
+    # lets user enter moves 
+    def query_player(game, state):
+        print("Current board state:")
+        game.display(state) # displays current game state 
+        print("Available valid moves: {}".format(game.actions(state)))  # shows valid list of moves 
+        print("")
+        move = None # initialize move to None
+        # if there are valid moves for current board 
+        if game.actions(state):
+            move_string = input('Your move? ')  # take user input for move 
+            try:
+                row, col = move_string.split(',')
+                move = (int(row), int(col))
+               # if input entered by user is invalid  
+            except ValueError:
+                move = move_string
+        else:
+            print('no valid moves: passing turn to next player')
+        return move
+    
+# function that sets up the game between players 
+def play_game():
+    play_again = True  # initialize play_again to True
+
+    while play_again:  # loop until user does not want to play again 
+        game = NewConnectFour() # create new game instance 
+        state = game.initial
+
+        # user picks whether to play against AI or human player 
+        print("Welcome to ConnectFour! Let's get started!")
+        players = {'X': 'human', 'O': 'human'}
+        game_option = input("Enter 'A' to play against the AI opponent or 'H' to play against another human: ")
+        if game_option.lower() == 'a':
+            players['O'] = 'ai'
+            print("You are playing against an AI!")
+        else:
+            print("You are playing against another human.")
+
+        while True:
+            current_player = game.to_move(state)
+            print("Current player: {}".format(current_player))  # shows which player has to move
+            game.display(state)
+            print("Available moves: {}".format(game.actions(state)))    # shows valid mvoes 
             print("")
+            if players[current_player] == 'human':
+                move = query_player(game, state)    # calls query_player function for human player 
+            else:
+                start_time = time.time()
+                move = alpha_beta_player(game, state)   # calls alpha_beta_player function w/ alpha beta cutoff search
+                end_time = time.time()
+                print("Time taken by AI player: {:.3f} seconds".format(end_time - start_time))  # prints AI execution time per move
+            if move is None:
+                break
+            state = game.result(state, move)
+            print("Score:", game.evaluate(state))   # prints score from evaluate function
+            if game.terminal_test(state):   # game is over 
+                break
+
+        # calculate final score and print result
+        score = game.utility(state, game.to_move(game.initial)) # uses utility function to calculate who won 
+        if score == 1:
+            print("Player X has won the game!")
+        elif score == -1:
+            print("Player O has won the game!")
+        else:
+            print("Aww...it's a draw")
+
+        # ask the user if they want to play again
+        play_again = input("Do you want to play again? (y/n)").lower() in ('y')
+        print("")
+
+    print("Thanks for playing ConnectFour!")
+
 
 if __name__ == "__main__":
-    game = ConnectFour()
-    print("Welcome to Connect4! Let's get started!")
-    board = {(row, col): "." for row in range(game.rows) for col in range(game.cols)}
-    game.initial = GameState(player_to_move='X', utility=game.compute_utility(board, None, 'X'), board=board, moves=[])
-    state = game.initial
-    player_choice = input("Would you like to play against a human player (H) or against an AI opponent (A)? ").upper()
-    while player_choice not in ["H", "A"]:
-        player_choice = input("Invalid choice. Please enter H or A only: ").upper()
-    while not game.terminal_test(state):
-        game.display(state)
-        if state.to_move() == 'X':
-            col = int(input("Player X's turn. Enter a column number: "))
-            state = game.result(state, row=None, col=col)
-        else:
-            if player_choice == "H":
-                col = int(input("Player O's turn. Enter a column number: "))
-                state = game.result(state, row=None, col=col)
-            else:
-                state = alpha_beta_player(game, state)
-        if not game.terminal_test(state):
-            game.display(state)
-            if state.to_move() == 'X':
-                if player_choice == "H":
-                    col = int(input("Player X's turn. Enter a column number: "))
-                    state = game.result(state, row=None, col=col)
-                else:
-                    state = alpha_beta_player(game, state)
-            else:
-                if player_choice == "H":
-                    col = int(input("Player O's turn. Enter a column number: "))
-                    state = game.result(state, row=None, col=col)
-                else:
-                    state = alpha_beta_player(game, state)
-    game.display(state)
-    utility = game.compute_utility(state.board, None, state.to_move)
-    if utility == 1:
-        if player_choice == "H":
-            print("Congratulations! Player X won!")
-        else:
-            print("Sorry, you lost...")
-    elif utility == -1:
-        if player_choice == "H":
-            print("Congratulations! Player O won!")
-        else:
-            print("Congratulations! You won!")
-    else:
-        print("It's a Draw.")
+    #game = NewConnectFour()
+    play_game()
+
 
